49ee9c34b1f45a9cbc32e9dfe2365030b817a49c
Syafiq Naim <syafiqnaim@Syafiqs-MBP.realtek>
2022-05-10 15:09:05 +0800

    test

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-echo "hello sir again v12";
+echo "hello sir again v13";
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
